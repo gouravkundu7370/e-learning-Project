bdc20023e6e193248ad54f6f30eefd60ff9447b1
Gourav Kundu <gouravkundu2902@gmail.com>
2023-11-29 03:44:37 +0530

    Added Authentication functions

---
+++ elearningplatform/app/(components)/Inputs/Input.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+export default function Input({
+  type,
+  value,
+  onChange,
+  name,
+  textarea,
+  id,
+  placeholder,
+  big,
+}) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      value={value}
+      onChange={onChange}
+      name={name}
+      className={`w-full p-4 pt-6 font-light bg-white border-2 outline-none text-black
+         ${textarea ? "w-700px h-500px" : "w-full"}
+         ${big ? "w-[700px] pb-[1rem]" : ""}`}
+    />
+  );
+}
